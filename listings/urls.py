f8520e5fd1ea5dc1533c8d66f5ec950bea775811
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-10 15:42:03 +0200

    Update urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -16,6 +16,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('listings.urls')),
+    path('api/', include('listings.urls')),  # include app router
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
